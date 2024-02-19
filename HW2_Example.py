a98f4ffc12c403afd64ab4455773950fc45123e0
Mallory Milstead <mallorymilstead@gmail.com>
2024-02-19 16:32:12 +0000

    first upload

---
+++ HW2_Example.py
@@ -0,0 +1,20 @@
+#Name
+#Date
+#Input and Output
+
+'''
+Pseudocode:
+Get budget (int)
+Get destination (str)
+Get gas (int)
+Get hotel (int)
+Get food (int)
+Add expenses
+budget - expenses
+Display results - use example
+'''
+
+print("This program caluclates and displays travel expeneses")
+# Get budget from user
+budget = int(input("Enter budget: "))
+dest = input("Enter your destination: ")
