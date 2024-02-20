55a15c7ef34ea619c53064aac34babe38ceac4a8
Mallory Milstead <mallorymilstead@gmail.com>
2024-02-20 15:23:06 +0000

    second commit

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 #Hard coded - value same everytime program runs
-student1 = "Mary"
+student1 = input("Enter a student's name")
 
 #User input - value is assigned the user's input
-student2 = input("Enter a student's name")
+student2 = input("Enter another student's name")
 
 print("The first student's name is", student1 )
 print("The second student's name is", student2 )
