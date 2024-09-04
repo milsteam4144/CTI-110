9ef7c56b65bd4c5fb5fddabd54ed0f12e253f2a7
Mallory Milstead <mallorymilstead@gmail.com>
2024-09-04 17:58:31 +0000

    Change the file

---
+++ P1LAB1.py
@@ -0,0 +1,13 @@
+# Mallory Milstead
+# 9/4/2024
+# P1LAB1
+# Input and Output
+
+# The input function always saves the data as a String by default
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print()
+print("Hello", first_name, last_name + "!", "Welcome to CTI-110.")
+print()
+print(type(first_name))
+
