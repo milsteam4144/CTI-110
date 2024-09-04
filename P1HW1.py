bebdd7905b0880d5615157de38eca443ad90b578
Mallory Milstead <mallorymilstead@gmail.com>
2024-09-04 18:36:31 +0000

    Changed the file

---
+++ P1HW1.py
@@ -0,0 +1,21 @@
+# Mallory Milstead
+# 9/4/2024
+# P1HW1
+# Input and Output with Integers
+
+print("----Calculating Exponents-----")
+print()
+print()
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(base_value, "raised to the power of", exponent, "is", base_value**exponent, "!!")
+print()
+print()
+print("-----Addition and Subtraction----")
+my_num = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print()
+print(my_num, "+", add, "-", subtract, "is equal to", my_num + add - subtract)
