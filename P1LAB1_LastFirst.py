7fd59eb7d2ac8983a558ab71ecf76a21815314b4
Mallory Milstead <mallorymilstead@gmail.com>
2024-09-24 14:21:17 -0400

    First upload

--- P1LAB1_LastFirst.py
+++ P1LAB1_LastFirst.py
@@ -1,13 +1,17 @@
-# M Milstead
-# 9/16/2024
+# Name
+# 9/24/2024
 # P1LAB1
-# Get input from user and display output
+# get input from the user and display output
 
 # Get first name from user
 first_name = input("Enter your first name: ")
 
-# Get last name from user
+# Get the last name from user
 last_name = input("Enter your last name: ")
 
 # Display info to user
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+print()
 print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
+
+
