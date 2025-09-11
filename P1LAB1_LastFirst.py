3d01fa09a4bc88e6f11a5ec63eb59a97b00dd5bc
Mallory Milstead <mallorymilstead@gmail.com>
2025-09-11 11:22:00 -0400

    Add files via upload

--- P1LAB1_LastFirst.py
+++ P1LAB1_LastFirst.py
@@ -1,17 +1,22 @@
-# Name
-# 9/24/2024
+# Mallory Milstead
+# 9/11/2025
 # P1LAB1
-# get input from the user and display output
+# Get input from user and display back to them
 
-# Get first name from user
+
+# Get first and last name from user
 first_name = input("Enter your first name: ")
-
-# Get the last name from user
 last_name = input("Enter your last name: ")
+print()
+# print(type(first_name))
+print()
+# Display greeting using first_name and last_name variables
 
-# Display info to user
+# Use concatenation
 print("Hello,", first_name, last_name + "! Welcome to CTI-110")
 print()
+
+# Use a comma
 print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
 
 
