ef5952852cd0bd2e0ba8cf13133c560f7d3111e5
Mallory Milstead <mallorymilstead@gmail.com>
2026-01-29 15:35:17 +0000

    Added user input for name

--- hello_students.py
+++ hello_students.py
@@ -1,3 +1,9 @@
 # printing hello to my students
 print()
 print("Hello guys. We gonna learn today!🖥️💿⌨️")
+
+# Create a variable to hold the user's name and get their input
+name = input("Enter your name: ")
+
+# Say hi to the user
+print("Hello my friend,", name)
